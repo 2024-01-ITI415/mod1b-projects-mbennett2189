41817b8d3c7f13ce5352e8a310e5a0920f26fe97
mbennett2189 <158121795+mbennett2189@users.noreply.github.com>
2024-02-12 23:31:09 -0500

    added cloud and script

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Dynamic")]
     public float camZ;
 
@@ -16,13 +20,32 @@ public class FollowCam : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (POI == null) return;
+        Vector3 destination = Vector3.zero;
 
-        Vector3 destination = POI.transform.position;
+        if (POI != null)
+        {
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
+
+            destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
 
         destination.y = camZ;
 
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
 
